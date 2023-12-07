ab4e7758803d3db573c451cad3d1c64fe828a5bf
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2023-12-07 17:53:21 +0900

    upload

---
+++ Assets/Scripts/Boss/BossEffectBehaviour.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEffectBehaviour : BossStateMachineBehaviour
+{
+    [SerializeField] private GameObject Effect;
+    [SerializeField] private int ActivateFrame;
+    [SerializeField] private int DisabledFrame;
+
+    private bool _isActivated;
+
+    private bool _isDisabled;
+
+    private AnimationClip _clip;
+    private int _currentFrame;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        float animeLength = stateInfo.length;
+        _boss.AddWaingTimer(animeLength);
+        _clip = animator.GetCurrentAnimatorClipInfo(layerIndex)[0].clip;
+    }
+
+
+    public override void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        _currentFrame = (int)(stateInfo.normalizedTime * (_clip.length * _clip.frameRate));
+
+        if (_currentFrame == ActivateFrame && !_isActivated)
+        {
+            _isActivated = true;
+            Debug.Log("Ω√¿€");
+        }
+
+        else if (_currentFrame == DisabledFrame && !_isDisabled)
+        {
+            _isDisabled = true;
+        }
+    }
+
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex);
+        _isDisabled = false;
+        _isActivated = false;
+    }
+}
