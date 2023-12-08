9693bfd4cee8ba1fbb5aac1bfbbb5f5a3892eab9
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2023-12-08 18:03:30 +0900

    boss fix upload

--- Assets/Scripts/Boss/Particles/BossParticle.cs
+++ Assets/Scripts/Boss/Particles/BossParticle.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class BossParticle : MonoBehaviour
 {
+    [Header("카메라 흔들림 지속 시간")]
+    [SerializeField] private float _shakeDuration = 0.5f;
+
+    [Header("카메라 흔들림 진폭")]
+    [SerializeField] private float _shakeAmplitude = 1f;
+
+    [Header("카메라 흔들림 빈도")]
+    [SerializeField] private float _shakeFrequency = 0.5f;
+
 
     [SerializeField] protected float _disabledTime;
 
@@ -20,5 +29,6 @@ public class BossParticle : MonoBehaviour
     public virtual void Start()
     {
         Destroy(gameObject, _disabledTime);
+        CinemachineCamera.Instance.CameraShake(_shakeDuration, _shakeAmplitude, _shakeFrequency);
     }
 }
