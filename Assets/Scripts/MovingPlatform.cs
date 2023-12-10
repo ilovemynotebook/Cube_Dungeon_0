ccb68ae87ca94d451e27ae25acb4bc0a363f1096
RedDustK <emfoswu123@naver.com>
2023-12-10 17:21:43 +0900

    12.10 움직이는플랫폼

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class MovingPlatform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float speed = 2.0f;
+    public float distance = 5.0f;
+    public bool moveOnXAxis = true;
+    public bool moveOnYAxis = true;
+
+    private Vector3 initialPosition;
+
+    private void Start()
     {
-        
+        initialPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float newPosition = Mathf.PingPong(Time.time * speed, distance);
+
+        // 사용자가 선택한 축으로만 이동
+        Vector3 newPositionVector = new Vector3(
+            moveOnXAxis ? initialPosition.x + newPosition : transform.position.x,
+            moveOnYAxis ? initialPosition.y + newPosition : transform.position.y,
+            initialPosition.z
+        );
+
+        transform.position = newPositionVector;
     }
 }
