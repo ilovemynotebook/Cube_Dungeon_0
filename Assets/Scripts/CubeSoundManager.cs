2859a76fdd644fb2f5d329a52d599af3e2ea39e7
dlackdanr777 <64966089+dlackdanr777@users.noreply.github.com>
2023-12-11 10:38:45 +0900

    upload

---
+++ Assets/Scripts/CubeSoundManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum AudioType
+{
+    Background,
+    Effect,
+    Length
+}
+
+
+public class CubeSoundManager : SingletonHandler<CubeSoundManager>
+{
+
+
+    private AudioSource[] _audioSources;
+
+
+    public override void Awake()
+    {
+        base.Awake();
+
+        _audioSources = new AudioSource[(int)AudioType.Length - 1];
+
+
+        for(int i = 0, count = (int)AudioType.Length; i < count; i++)
+        {
+            GameObject source = new GameObject(Enum.GetName(typeof(AudioType), i));
+            source.AddComponent<AudioSource>();
+            source.transform.parent = transform;
+            _audioSources[i] = source.GetComponent<AudioSource>();
+        }
+
+    }
+
+
+}
